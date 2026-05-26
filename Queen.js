0fa7949ce63187c3207a675ad244aac723b637b7
Bartosz Wisniowski <drolek12@gmail.com>
2026-05-26 12:18:49 +0200

    hotfix:dodano ruchy krolowej

--- Queen.js
+++ Queen.js
@@ -1,12 +1,11 @@
-// Klasa reprezentująca królową
 class Queen {
     constructor() {
         this.type = "QUEEN";
     }
 
     validateMove(move) {
-        
-        // dokoncz implementacje
-        return false;
+        const dx = Math.abs(move.destinationX - move.sourceX);
+        const dy = Math.abs(move.destinationY - move.sourceY);
+        return (dx === dy && dx > 0) || (dx === 0 && dy > 0) || (dy === 0 && dx > 0);
     }
 }
